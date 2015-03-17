922f8388677551ac7851f95ff81792f111c817c7
Liz Sanchez <ElizabethSanchez@Lizs-MacBook-Pro.local>
2015-03-17 12:08:07 -0400

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Liz Sanchez on 3/17/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
